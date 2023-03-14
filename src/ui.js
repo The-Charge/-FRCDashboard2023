8615fd2263be9ee1a282f2f709c45ba711c0cd58
Grant Wang <grantoyt@gmail.com>
2023-03-13 20:11:16 -0400

    3/13 Handing it back to Sahiba

--- src/ui.js
+++ src/ui.js
@@ -16,7 +16,6 @@ let ui = {
 // Update camera every second. This is necessary because if the camera disconnects during the match, it will not automatically reconnect
 setInterval(() => {
     // TEST IMAGES:
-     //ui.camera1.style.backgroundImage = 'url("https://i.ytimg.com/vi/ekthcIHDt3I/maxresdefault.jpg")';
     ui.camera1.style.backgroundImage = 'url("http://10.26.19.22:1181/stream.mjpg")';
 }, 500);
 
@@ -27,14 +26,14 @@ NetworkTables.addKeyListener('/robot/time', (key, value) => {
 });
 
 // Error handler
-addEventListener('error',(ev)=>{
-    ipc.send('windowError',{mesg:ev.message,file:ev.filename,lineNumber:ev.lineno})
+addEventListener('error', (ev) => {
+    ipc.send('windowError', { mesg: ev.message, file: ev.filename, lineNumber: ev.lineno })
 });
 
 //Event Listeners:
 
 NetworkTables.addKeyListener('/SmartDashboard/High Gear', (key, value) => { //FINAL NETWORKTABLE VALUE
-    if(value == true) {
+    if (value == true) {
         ui.gearText.innerHTML = 'HIGH';
         ui.gearText.classList.add(on);
         ui.gearText.classList.remove(off);
@@ -45,21 +44,30 @@ NetworkTables.addKeyListener('/SmartDashboard/High Gear', (key, value) => { //FI
     }
 });
 NetworkTables.addKeyListener('/SmartDashboard/is Reversed', (key, value) => { //FINAL NETWORKTABLE VALUE
-    if(value == true) {
-        ui.reversedState.innerHTML = 'CONNECTED';
+    if (value == true) {
+        ui.reversedState.innerHTML = 'Reversed';
         ui.reversedState.classList.add(on);
         ui.reversedState.classList.remove(off);
     } else {
-        ui.reversedState.innerHTML = 'DISCONNECTED';
+        ui.reversedState.innerHTML = 'Normal';
         ui.reversedState.classList.add(off);
         ui.reversedState.classList.remove(on);
     }
 });
 
 NetworkTables.addKeyListener('/SmartDashboard/Pipeline', (key, value) => { //FINAL NETWORKTABLE VALUE
-    ui.pipelineState.textContent = value; 
-    pipelineState.value = value;
-    pipelineState.innerHTML = value;
+    if (value == true) {
+        ui.pipelineState.textContent = value;
+        pipelineState.value = value;
+        pipelineState.innerHTML = value;
+    } else {
+        ui.pipelineState.textContent = value;
+        pipelineState.value = value;
+        pipelineState.innerHTML = value;
+    }
+    //ui.reversedState.innerHTML = 'Normal';
+    //ui.reversedState.classList.add(off);
+    //ui.reversedState.classList.remove(on);
 });
 
 NetworkTables.addKeyListener('/SmartDashboard/Arm State', (key, value) => { //FINAL NETWORKTABLE VALUE
@@ -69,13 +77,13 @@ NetworkTables.addKeyListener('/SmartDashboard/Arm State', (key, value) => { //FI
 });
 
 NetworkTables.addKeyListener('/SmartDashboard/Half Speed', (key, value) => { //FINAL NETWORKTABLE VALUE
-    if(value == true) {
+    if (value == true) {
         ui.speedMode.textContent = 'HALF';
         ui.speedMode.innerHTML = 'HALF';
         speedMode.value = value;
     } else {
-        NetworkTables.addKeyListener('/SmartDashboard/Full Speed', (key,value) => {
-            if(value == true) {
+        NetworkTables.addKeyListener('/SmartDashboard/Full Speed', (key, value) => {
+            if (value == true) {
                 ui.speedMode.textContent = 'FULL';
                 ui.speedMode.innerHTML = 'FULL';
                 speedMode.value = value;
@@ -91,31 +99,31 @@ NetworkTables.addKeyListener('/SmartDashboard/Half Speed', (key, value) => { //F
 
 -
 
-// UNTESTED:
-// Load list of prewritten autonomous modes
-NetworkTables.addKeyListener('/SmartDashboard/AutoSelect', (key, values) => { //FINAL NETWORKTABLE VALUE
-    // Clear previous list
-    // while (ui.input.autoSelect.firstChild) {
-    //     ui.input.autoSelect.removeChild(ui.input.autoSelect.firstChild);
-    // }
-    // Make an option for each autonomous mode and put it in the selector
-    // for (let i = 0; i < value.length; i++) {
-    //     var option = document.createElement('option');
-    //     option.appendChild(document.createTextNode(value[i]));
-    //     ui.input.autoSelect.appendChild(option);
-    // }
-    // // Set value to the already-selected mode. If there is none, nothing will happen.
-    // ui.input.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
+    // UNTESTED:
+    // Load list of prewritten autonomous modes
+    NetworkTables.addKeyListener('/SmartDashboard/AutoSelect', (key, values) => { //FINAL NETWORKTABLE VALUE
+        // Clear previous list
+        // while (ui.input.autoSelect.firstChild) {
+        //     ui.input.autoSelect.removeChild(ui.input.autoSelect.firstChild);
+        // }
+        // Make an option for each autonomous mode and put it in the selector
+        // for (let i = 0; i < value.length; i++) {
+        //     var option = document.createElement('option');
+        //     option.appendChild(document.createTextNode(value[i]));
+        //     ui.input.autoSelect.appendChild(option);
+        // }
+        // // Set value to the already-selected mode. If there is none, nothing will happen.
+        // ui.input.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
 
-    while(ui.autoSelect.options.length > 0){
-    	ui.autoSelect.remove(0);
-    }
-  for (let i = 0; i < values.length; i++) {
-        var option = document.createElement('option');
-        option.text = values[i].getName();
-        option.value = values[i];
-        ui.autoSelect.add(option,null);
-    }
-    // Set value to the already-selected mode. If there is none, nothing will happen.
-    //ui.input.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
-});
+        while (ui.autoSelect.options.length > 0) {
+            ui.autoSelect.remove(0);
+        }
+        for (let i = 0; i < values.length; i++) {
+            var option = document.createElement('option');
+            option.text = values[i].getName();
+            option.value = values[i];
+            ui.autoSelect.add(option, null);
+        }
+        // Set value to the already-selected mode. If there is none, nothing will happen.
+        //ui.input.autoSelect.value = NetworkTables.getValue('/SmartDashboard/currentlySelectedMode');
+    });
